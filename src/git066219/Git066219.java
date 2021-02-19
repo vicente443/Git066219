affa1dee1f7d1f5cda1a2f018b68e8e2245f5188
chent <chent@LAPTOP-MPI3AJB6>
2021-02-18 20:20:10 -0600

    Estoy igualando mis dos copias, el workink copy y el local repository

---
+++ src/git066219/Git066219.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package git066219;
+
+/**
+ *
+ * @author chent
+ */
+public class Git066219 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nombre = "Vicente Damian";
+        String matricula = "066219";
+            System.out.println("soy " + nombre + "y mi matricula es " + matricula);
+            
+    }
+    
+}
